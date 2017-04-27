b4aee073f5610ba82142b2c93edc39375fcf2b03
Isaac Daniel Batista <disa.indus@gmail.com>
2017-04-27 14:49:18 -0500

    Definicion de rutas, render html, paso de variables a las vistas de blade

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -79,17 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laratter by Platzi
-                </div>
-                @if (isset($teacher))
-                    <p>Profesor: {{ $teacher }} </p>
-                @else
-                    <p>Profesor a definir</p>
-                @endif
-                <div class="links">
-                    @foreach ($links as $link => $text)
-                        <a href="{{ $link }}">{{ $text }}</a>
-                    @endforeach
+                    Acerca de nosotros
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,16 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $links['https://laravel.com/docs'] ='Documentation';
+    $links['https://laracasts.com'] ='Laracasts';
+    $links['https://laravel-news.com'] ='News';
+    $links['https://forge.laravel.com'] ='Forge';
+    $links['https://github.com/laravel/laravel'] ='GitHub';
+    $links['https://about.me/idanibat'] = 'Isaac Batista';
+    $teacher = 'Guido Contreras Woda';
+    return view('welcome', compact('links', 'teacher'));
+});
+
+Route::get('/acerca', function () {
+    return view('about');
 });

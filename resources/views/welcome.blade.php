096e3d205caf34b9ca2a13d57f0246245e6f20dd
Isaac Daniel Batista <disa.indus@gmail.com>
2017-05-03 08:36:24 -0500

    Creacion de modelo, uso basico de ORM, se modifica la plantilla para usar objetos en vez de arrays

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,9 +13,9 @@
     <div class="row">
         @forelse ($messages as $message)
             <div class="col-6">
-                <img class="img-thumbnail" src="{{ $message['image'] }}">
-                <p class="card-text">{{ $message['content'] }}
-                    <a href="/messages/{{ $message['id'] }}">Leer más</a>
+                <img class="img-thumbnail" src="{{ $message->image }}">
+                <p class="card-text">{{ $message->content }}
+                    <a href="/messages/{{ $message->id }}">Leer más</a>
                 </p>
             </div>
         @empty

25231c1edc10dd66e05235b3afe3a155664e28e8
Isaac Daniel Batista <disa.indus@gmail.com>
2017-05-04 08:49:04 -0500

    Se crea un controlador para los mensajes, asi como su ruta y su vista para visualizar un mensaje por id

---
+++ app/Http/Controllers/MessagesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Message;
+use Illuminate\Http\Request;
+
+class MessagesController extends Controller
+{
+    public function show ($id)
+    {
+    	$message = Message::find($id);
+    	return view('messages.show', compact('message'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,4 @@
 */
 
 Route::get('/', 'PagesController@home');
-
+Route::get('/messages/{message}', 'MessagesController@show');

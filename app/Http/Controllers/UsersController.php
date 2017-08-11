280d805bada2883110c706cb33c704e2475775e7
Isaac Daniel Batista <disa.indus@gmail.com>
2017-08-11 01:28:39 -0500

    Mostrando mensajes en la aplicacion

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Conversation;
+use App\PrivateMessage;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -41,6 +43,31 @@ class UsersController extends Controller
         $me = $request->user();
         $me->follows()->detach($user);
         return redirect("/$username")->withSuccess('Usuario no seguido!');
+    }
+
+    public function sendPrivateMessage($username, Request $request)
+    {
+        $user = $this->findByUsername($username);
+        $me = $request->user();
+        $message = $request->input('message');
+        $conversation = Conversation::create();
+        $conversation->users()->attach($me);
+        $conversation->users()->attach($user);
+        $privateMessage = PrivateMessage::create([
+            'conversation_id' => $conversation->id,
+            'user_id' => $me->id,
+            'message' => $message,
+        ]);
+
+        return redirect('/conversations/'.$conversation->id);
+    }
+
+    public function showConversation(Conversation $conversation, Request $request)
+    {
+        $user = auth()->user();
+        $conversation->load('users', 'privateMessages');
+        
+        return view('users.conversation', compact('conversation', 'user'));
     }
 
     private function findByUsername($username)
